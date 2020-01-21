9392356f98caf4e70b871674bc53b455c5681e83
Andrew <bohdan.andrii@gmail.com>
2020-01-21 15:56:54 -0500

    D2: Model created

---
+++ MCalc/app/src/main/java/com/andriibohdan/mcalc/MortgageModel.java
@@ -0,0 +1,30 @@
+package com.andriibohdan.mcalc;
+
+public class MortgageModel {
+    private double principle;
+    private double amortization;
+    private double interest;
+    private double monthlyPayment;
+
+    public MortgageModel(String p, String a, String i) {
+        principle = Double.parseDouble(p);
+        amortization = Double.parseDouble(a);
+        interest = Double.parseDouble(i)/100;
+    }
+
+    public double computePayment() {
+        return monthlyPayment = (principle * interest)/(1-Math.pow(1+interest, -amortization*12));
+    }
+
+    public double getMonthlyPayment() {
+        return monthlyPayment;
+    }
+
+    public static void main(String args[]) {
+        MortgageModel m1 = new MortgageModel("700000", "25", "2.75");
+        System.out.println(m1.computePayment());
+
+        MortgageModel m2 = new MortgageModel("300000", "20", "4.5");
+        System.out.println(m2.computePayment());
+    }
+}
