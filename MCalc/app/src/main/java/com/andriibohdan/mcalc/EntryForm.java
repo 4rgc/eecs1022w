c56012c5f5f0926240e16fcac476d1f64cd0ab9f
Andrew <bohdan.andrii@gmail.com>
2020-01-21 14:07:37 -0500

    D2: Created project

---
+++ MCalc/app/src/main/java/com/andriibohdan/mcalc/EntryForm.java
@@ -0,0 +1,14 @@
+package com.andriibohdan.mcalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EntryForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mortgage_layout);
+    }
+}
